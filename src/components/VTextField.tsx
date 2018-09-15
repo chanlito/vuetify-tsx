9991f71693f145029f46fdf772f6bf5e7e34a444
Chanlito <chanlitohem@gmail.com>
2018-09-15 18:34:23 +0700

    :sparkles: add input props

--- src/components/VTextField.tsx
+++ src/components/VTextField.tsx
@@ -1,22 +1,68 @@
 import { ofType } from 'vue-tsx-support';
 
-import { CommonEvents } from '../types';
+import {
+  Colorable,
+  CommonEvents,
+  Disableable,
+  Measurable,
+  Themable,
+} from '../types';
 
 export default ofType<Props, Events>().convert(
   require('vuetify/es5/components/VTextField').VTextField,
 );
 
-interface Props {
-  errorMessages?: string;
-  flat?: boolean;
-  hideDetails?: boolean;
-  label?: string;
-  name?: string;
-  prependIcon?: string;
-  readonly?: boolean;
-  singleLine?: boolean;
-  solo?: boolean;
-  value?: string;
-}
+type Props = Colorable &
+  Disableable &
+  Measurable &
+  Themable & {
+    appendIcon?: string;
+    appendIconCb?: Function;
+    appendOuterIcon?: Function;
+    appendOuterIconCb?: Function;
+    autofocus?: boolean;
+    backgroundColor?: string;
+    box?: boolean;
+    browserAutocomplete?: string;
+    clearIcon?: string;
+    clearIconCb?: Function;
+    clearable?: boolean;
+    counter?: boolean | number | string;
+    error?: boolean;
+    errorCount?: string | number;
+    errorMessages?: string;
+    flat?: boolean;
+    fullWidth?: boolean;
+    hideDetails?: boolean;
+    hint?: string;
+    label?: string;
+    loading?: boolean | string;
+    mask?: string | object;
+    messages?: string | string[];
+    outline?: boolean;
+    persistentHint?: boolean;
+    name?: string;
+    placeholder?: string;
+    prefix?: string;
+    prependIcon?: string;
+    prependIconCb?: Function;
+    prependInnerIcon?: string;
+    prependInnerIconCb?: Function;
+    readonly?: boolean;
+    returnMaskedValue?: boolean;
+    reverse?: boolean;
+    rules: ((...args: any[]) => boolean | string)[];
+    singleLine?: boolean;
+    solo?: boolean;
+    soloInverted?: boolean;
+    success?: boolean;
+    successMessages?: string | string[];
+    suffix?: string;
+    /** @deprecated */
+    textarea?: boolean;
+    type?: string;
+    validateOnBlur?: boolean;
+    value?: any;
+  };
 
 type Events = CommonEvents;
--- src/components/VTextarea.tsx
+++ src/components/VTextarea.tsx
@@ -1,9 +1,71 @@
 import { ofType } from 'vue-tsx-support';
 
+import {
+  Colorable,
+  CommonEvents,
+  Disableable,
+  Measurable,
+  Themable,
+} from '../types';
+
 export default ofType<Props, Events>().convert(
   require('vuetify/es5/components/VTextarea').VTextarea,
 );
 
-type Props = {};
+type Props = Colorable &
+  Disableable &
+  Measurable &
+  Themable & {
+    appendIcon?: string;
+    appendIconCb?: Function;
+    appendOuterIcon?: Function;
+    appendOuterIconCb?: Function;
+    autofocus?: boolean;
+    backgroundColor?: string;
+    box?: boolean;
+    browserAutocomplete?: string;
+    clearIcon?: string;
+    clearIconCb?: Function;
+    clearable?: boolean;
+    counter?: boolean | number | string;
+    error?: boolean;
+    errorCount?: string | number;
+    errorMessages?: string;
+    flat?: boolean;
+    fullWidth?: boolean;
+    hideDetails?: boolean;
+    hint?: string;
+    label?: string;
+    loading?: boolean | string;
+    mask?: string | object;
+    messages?: string | string[];
+    outline?: boolean;
+    persistentHint?: boolean;
+    name?: string;
+    placeholder?: string;
+    prefix?: string;
+    prependIcon?: string;
+    prependIconCb?: Function;
+    prependInnerIcon?: string;
+    prependInnerIconCb?: Function;
+    readonly?: boolean;
+    returnMaskedValue?: boolean;
+    reverse?: boolean;
+    rules: ((...args: any[]) => boolean | string)[];
+    singleLine?: boolean;
+    solo?: boolean;
+    soloInverted?: boolean;
+    success?: boolean;
+    successMessages?: string | string[];
+    suffix?: string;
+    /** @deprecated */
+    textarea?: boolean;
+    type?: string;
+    validateOnBlur?: boolean;
+    value?: any;
+    noResize?: boolean;
+    rowHeight?: number | string;
+    rows?: number | string;
+  };
 
-type Events = {};
+type Events = CommonEvents;
