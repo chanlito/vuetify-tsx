0ee46f60273727f393f892e56589b3568992ee86
Chanlito <chanlitohem@gmail.com>
2018-09-16 16:20:13 +0700

    :bug: wrong import

--- src/components/VBreadcrumbsItem.tsx
+++ src/components/VBreadcrumbsItem.tsx
@@ -3,7 +3,7 @@ import { ofType } from 'vue-tsx-support';
 import { Disableable, Routable } from '../types';
 
 export default ofType<Props, Events>().convert(
-  require('vuetify/es5/components/VBreadcrumbsItem').VBreadcrumbsItem,
+  require('vuetify/es5/components/VBreadcrumbs').VBreadcrumbsItem,
 );
 
 type Props = Disableable & Routable & {};
--- src/components/VCarouselItem.tsx
+++ src/components/VCarouselItem.tsx
@@ -3,7 +3,7 @@ import { ofType } from 'vue-tsx-support';
 import { Transitionable } from '../types';
 
 export default ofType<Props, Events>().convert(
-  require('vuetify/es5/components/VCarouselItem').VCarouselItem,
+  require('vuetify/es5/components/VCarousel').VCarouselItem,
 );
 
 type Props = Transitionable & {
