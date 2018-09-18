ab084c5aac9a576b631735f46546be3d6d67a8fe
Chanlito <chanlitohem@gmail.com>
2018-09-18 16:48:25 +0700

    :sparkles: add props

--- src/components/VLayout.tsx
+++ src/components/VLayout.tsx
@@ -1,5 +1,11 @@
 import { ofType } from 'vue-tsx-support';
 
-export default ofType().convert(
+export default ofType<Props>().convert(
   require('vuetify/es5/components/VGrid').VLayout,
 );
+
+type Props = {
+  column?: boolean;
+  row?: boolean;
+  wrap?: boolean;
+};
