9f607c43de82d0b31e5c05960c508a5d60caf144
Chanlito <chanlitohem@gmail.com>
2018-09-25 10:57:49 +0700

    :recycle: use relative import

--- src/components/VTooltip.tsx
+++ src/components/VTooltip.tsx
@@ -1,5 +1,6 @@
-import { Positionable } from '@/types';
 import { ofType } from 'vue-tsx-support';
+
+import { Positionable } from '../types';
 
 export default ofType<Props>().convert(
   require('vuetify/es5/components/VTooltip').VTooltip,
