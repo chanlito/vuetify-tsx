004c2a0ecebf14f1b216dab4e9d9eda4d444975e
Chanlito <chanlitohem@gmail.com>
2018-09-26 10:03:05 +0700

    :recycle: update props for VMenu

--- src/components/VMenu.tsx
+++ src/components/VMenu.tsx
@@ -1,12 +1,47 @@
 import { ofType } from 'vue-tsx-support';
 
+import {
+  Detachable,
+  Disableable,
+  Positionable,
+  Themable,
+  Transitionable,
+} from '../types';
+
 export default ofType<Props>().convert(
   require('vuetify/es5/components/VMenu').VMenu,
 );
 
-type Props = {
-  left?: boolean;
-  right?: boolean;
-  top?: boolean;
-  bottom?: boolean;
-};
+type Props = Detachable &
+  Disableable &
+  Positionable &
+  Themable &
+  Transitionable & {
+    allowOverflow?: boolean;
+    auto?: boolean;
+    closeDelay?: number | string;
+    closeOnClick?: boolean;
+    closeOnContentClick?: boolean;
+    fullWidth?: boolean;
+    inputActivator?: boolean;
+    lazy?: boolean;
+    maxHeight?: number | string;
+    maxWidth?: number | string;
+    minWidth?: number | string;
+    nudgeBottom?: number | string;
+    nudgeLeft?: number | string;
+    nudgeRight?: number | string;
+    nudgeTop?: number | string;
+    offsetOverflow?: boolean;
+    offsetX?: boolean;
+    offsetY?: boolean;
+    openDelay?: number | string;
+    openOnClick?: boolean;
+    openOnHover?: boolean;
+    origin?: string;
+    positionX?: number;
+    positionY?: number;
+    returnValue?: any;
+    value?: any;
+    zIndex?: number | string;
+  };
