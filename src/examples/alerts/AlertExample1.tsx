003f60cc611c8753c88f5a8e259bb135a50f94ae
Christopher Quadflieg <chrissi92@hotmail.de>
2019-06-09 20:17:24 +0200

    Move examples to a new folder

---
+++ src/examples/alerts/AlertExample1.tsx
@@ -0,0 +1,19 @@
+import VAlert from '@/components/VAlert';
+import { component } from 'vue-tsx-support';
+import SubTitle from '../SubTitle';
+
+const AlertExample1 = component({
+  render() {
+    return (
+      <div>
+        <SubTitle>Alert</SubTitle>
+        <VAlert color="success" value={true}>
+          Awesome!
+        </VAlert>
+        <VAlert value={true}>Oh !@#$!</VAlert>
+      </div>
+    );
+  },
+});
+
+export default AlertExample1;
