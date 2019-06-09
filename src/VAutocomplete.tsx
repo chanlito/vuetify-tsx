20c56195e163b98b40295d4adea4c7024c3682ce
Christopher Quadflieg <chrissi92@hotmail.de>
2019-06-09 17:31:09 +0200

    Add ScopedSlot

--- src/VAutocomplete.tsx
+++ src/VAutocomplete.tsx
@@ -1,9 +1,32 @@
+import { Component as VueComponent } from 'vue';
 import { ofType } from 'vue-tsx-support';
 import { VAutocomplete } from 'vuetify/lib';
-import { Inputable } from './types';
+import { Colorable, Inputable } from './types';
 
-export default ofType<Props>().convert(VAutocomplete as any);
+export default ofType<Props, Events, ScopedSlot>().convert(
+  VAutocomplete as any,
+);
 
-type Props = Inputable & {
-  items: any[];
+type Props = Colorable &
+  Inputable & {
+    'v-model'?: any;
+    items: any[];
+    'item-text': string | ((value: any) => string);
+    multiple?: boolean;
+    chips?: boolean;
+  };
+
+type Events = {};
+
+type ScopedSlot = {
+  selection: Selection;
+  item: any;
+};
+
+type Selection = {
+  parent: VueComponent;
+  item: any;
+  index: number;
+  selected: boolean;
+  disabled: boolean;
 };
