805b43a589bd49afb90919d0d7140dbe07c61abe
Chanlito <chanlitohem@gmail.com>
2019-06-10 21:29:07 +0700

    chore: :fire: remove commented

--- src/App.tsx
+++ src/App.tsx
@@ -91,29 +91,10 @@ const App = component({
           onInput={this.updateDrawer}
         >
           <VList color="primary">
-            {this.menu.map(
-              ({ icon, label, to, exact, menu }) =>
-                menu && menu.length ? (
-                  <VListGroup noAction value={true}>
-                    <VListTile slot="activator" to={to} exact={exact}>
-                      <VListTileAction>
-                        <VIcon>{icon}</VIcon>
-                      </VListTileAction>
-                      <VListTileContent>
-                        <VListTileTitle>{label}</VListTileTitle>
-                      </VListTileContent>
-                    </VListTile>
-
-                    {menu.map(subMenu => (
-                      <VListTile to={subMenu.to}>
-                        <VListTileContent>
-                          <VListTileTitle>{subMenu.label}</VListTileTitle>
-                        </VListTileContent>
-                      </VListTile>
-                    ))}
-                  </VListGroup>
-                ) : (
-                  <VListTile to={to} exact={exact}>
+            {this.menu.map(({ icon, label, to, exact, menu }) =>
+              menu && menu.length ? (
+                <VListGroup noAction value={true}>
+                  <VListTile slot="activator" to={to} exact={exact}>
                     <VListTileAction>
                       <VIcon>{icon}</VIcon>
                     </VListTileAction>
@@ -121,19 +102,25 @@ const App = component({
                       <VListTileTitle>{label}</VListTileTitle>
                     </VListTileContent>
                   </VListTile>
-                ),
-              // menu && menu.length ? (
-              //   <VListTile slot="activator" to={to} exact={exact}>
-              //     <VListTileAction>
-              //       <VIcon>{icon}</VIcon>
-              //     </VListTileAction>
-              //     <VListTileContent>
-              //       <VListTileTitle>{label}</VListTileTitle>
-              //     </VListTileContent>
-              //   </VListTile>
-              // ) : (
 
-              // ),
+                  {menu.map(subMenu => (
+                    <VListTile to={subMenu.to}>
+                      <VListTileContent>
+                        <VListTileTitle>{subMenu.label}</VListTileTitle>
+                      </VListTileContent>
+                    </VListTile>
+                  ))}
+                </VListGroup>
+              ) : (
+                <VListTile to={to} exact={exact}>
+                  <VListTileAction>
+                    <VIcon>{icon}</VIcon>
+                  </VListTileAction>
+                  <VListTileContent>
+                    <VListTileTitle>{label}</VListTileTitle>
+                  </VListTileContent>
+                </VListTile>
+              ),
             )}
           </VList>
         </VNavigationDrawer>
@@ -141,44 +128,5 @@ const App = component({
     );
   },
 });
-
-// import AlertExample1 from './examples/alerts/AlertExample1';
-// import AvatarExample1 from './examples/avatars/AvatarExample1';
-// import BadgeExample1 from './examples/badges/BadgeExample1';
-// import BreadcrumbExample1 from './examples/breadcrumbs/BreadcrumbExample1';
-// import ButtonExample1 from './examples/buttons/ButtonExample1';
-// import ButtonToggleExample1 from './examples/buttons/ButtonToggleExample1';
-// import CardExample1 from './examples/cards/CardExample1';
-// import Title from './examples/Title';
-// import { VApp, VContainer, VContent, VFlex, VLayout } from './index';
-
-// const App = component({
-//   name: 'App',
-//   render() {
-//     return (
-//       <VApp>
-//         <VContent>
-//           <h1 class="title">Hello World</h1>
-//           <VContainer>
-//             <VLayout justify-center>
-//               <Title>Vuetify TSX Components</Title>
-//             </VLayout>
-//             <VLayout>
-//               <VFlex>
-//                 <AlertExample1 />
-//                 <AvatarExample1 />
-//                 <BadgeExample1 />
-//                 <BreadcrumbExample1 />
-//                 <ButtonExample1 />
-//                 <ButtonToggleExample1 />
-//                 <CardExample1 />
-//               </VFlex>
-//             </VLayout>
-//           </VContainer>
-//         </VContent>
-//       </VApp>
-//     );
-//   },
-// });
 
 export default App;
