2c7df46d699a4dac8aeebd366c6d944ef1d9b9f8
Christopher Quadflieg <chrissi92@hotmail.de>
2019-07-06 19:18:02 +0200

    Add CommonEvents

--- src/components/VForm.tsx
+++ src/components/VForm.tsx
@@ -1,7 +1,8 @@
+import { CommonEvents } from '@/shared/types';
 import { ofType } from 'vue-tsx-support';
 import { VForm } from 'vuetify/lib';
 
-export default ofType<Props>().convert(VForm as any);
+export default ofType<Props, CommonEvents>().convert(VForm as any);
 
 type Props = {
   ref?: string;
